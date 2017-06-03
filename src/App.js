e6f40b3cd1b3b5674b23d89e79f8eca634d08261
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2017-06-03 15:56:22 +0000

    chose betwen sidebarTypes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,62 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import OpenSideBar from './modules/OpenSideBar'
+import SideBar from './modules/SideBar'
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       collapsedBar: true,
+       staticBar: false
+     }
+     this.staticBarActive = this.staticBarActive.bind(this);
+     this.collapsedBarActive = this.collapsedBarActive.bind(this)
+  }
+
+  staticBarActive() {
+    this.setState ({
+      staticBar: true,
+      collapsedBar: false
+    })
+  }
+
+  collapsedBarActive() {
+    this.setState ({
+      collapsedBar: true,
+      staticBar: false
+    })
+  }
+
   render() {
+    const { collapsedBar, staticBar } = this.state
+
     return (
       <div className="App">
-        <div className="body">
-        <OpenSideBar />
+      <div>
+        { staticBar ? (
+          <div className="width-20">
+            <SideBar />
+          </div>
+        ) : '' }
+      </div>
+        <div className={ staticBar ? 'width-80' : 'width-100'}>
+            { collapsedBar ? ( <OpenSideBar /> ) : '' }
           <div className="intro-box">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1>A sideBar in React</h1>
+            <h1>A SideBar in React</h1>
+
+              <h4>Chose type:</h4>
+              <div className="btn-box">
+                <div onClick={this.collapsedBarActive} className={ collapsedBar ? 'bar-type-btn-active' : 'bar-type-btn'}>
+                  Collapsed
+                </div>
+                <div onClick={this.staticBarActive} className={ staticBar ? 'bar-type-btn-active' : 'bar-type-btn'}>
+                  Fixed
+                </div>
+              </div>
+
           </div>
         </div>
       </div>
--- src/modules/SideBar.js
+++ src/modules/SideBar.js
@@ -1,55 +1,51 @@
 import React, { Component } from 'react'
 import { FaNewspaperO, FaHome, FaUser, FaAngleRight } from 'react-icons/lib/fa'
 import './SideBar.css'
+import Dropdown from './Dropdown'
 
-class SideBar extends Component {
+class DropdownMenu extends Component {
   constructor(props){
     super(props);
       this.state = {
         dropdown: true,
-        dropdown2: true,
-        dropdown3: true
     }
     this.toggleDropdown = this.toggleDropdown.bind(this);
-    this.toggleDropdown2 = this.toggleDropdown2.bind(this);
-    this.toggleDropdown3 = this.toggleDropdown3.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('click', this.dropdown, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.dropdown, true);
   }
 
   toggleDropdown(){
     this.setState(prevState =>({
       dropdown: !prevState.dropdown,
-      dropdown2: true,
-      dropdown3: true
     }))
   }
 
-  toggleDropdown2(){
-    this.setState(prevState =>({
-      dropdown2: !prevState.dropdown2,
+  _handleBlur() {
+    // Clean up everything on blur
+    this.setState({
       dropdown: true,
-      dropdown3: true
-    }))
-  }
-
-  toggleDropdown3(){
-    this.setState(prevState =>({
-      dropdown3: !prevState.dropdown3,
-      dropdown2: true,
-      dropdown: true
-    }))
+    });
   }
 
   render(){
-    const { dropdown, dropdown2, dropdown3 } = this.state
+    const { dropdown } = this.state
 
     return (
       <div className="sidebar-menu">
-
-        <div className="sidebar-menu-item" onClick={this.toggleDropdown} >
+        <div className="sidebar-menu-item"
+            onClick={this.toggleDropdown}
+            onBlur={this._handleBlur} >
           <FaHome className="menu-icon" />
             <span>Item 01</span>
           <FaAngleRight className={ dropdown ? ('drop-icon') : ('drop-icon-active')} />
         </div>
+
             { dropdown ? (<div /> ) : (
               <div className="drop-menu-box">
                 <div className="drop-menu">
@@ -63,57 +59,23 @@ class SideBar extends Component {
               </div>
             )}
 
-
-        <div className="sidebar-menu-item" onClick={this.toggleDropdown2}>
-          <FaNewspaperO className="menu-icon" />
-            <span>Item 02</span>
-          <FaAngleRight className={ dropdown2 ? ('drop-icon') : ('drop-icon-active')} />
-        </div>
-          { dropdown2 ? (<div /> ) : (
-            <div className="drop-menu-box">
-              <div className="drop-menu">
-                <div className="drop-menu-item">
-                    <span>Drop-Item 03</span>
-                </div>
-                <div className="drop-menu-item">
-                    <span>Drop-Item 04</span>
-                </div>
-                <div className="drop-menu-item">
-                    <span>Drop-Item 05</span>
-                </div>
-                <div className="drop-menu-item">
-                    <span>Drop-Item 06</span>
-                </div>
-              </div>
-            </div>
-          )}
-
-          <div className="sidebar-menu-item" onClick={this.toggleDropdown3}>
-            <FaUser className="menu-icon" />
-              <span>Item 03</span>
-            <FaAngleRight className={ dropdown3 ? ('drop-icon') : ('drop-icon-active')} />
-          </div>
-            { dropdown3 ? (<div /> ) : (
-              <div className="drop-menu-box">
-                <div className="drop-menu">
-                  <div className="drop-menu-item">
-                      <span>Drop-Item 07</span>
-                  </div>
-                  <div className="drop-menu-item">
-                      <span>Drop-Item 08</span>
-                  </div>
-                  <div className="drop-menu-item">
-                      <span>Drop-Item 09</span>
-                  </div>
-                  <div className="drop-menu-item">
-                      <span>Drop-Item 10</span>
-                  </div>
-                </div>
-              </div>
-            )}
       </div>
     )
   }
 }
+
+class SideBar extends Component {
+  render() {
+    return (
+      <div>
+        <DropdownMenu />
+      </div>
+    )
+  }
+}
+
+
+
+
 
 export default SideBar;
